94cb36c16a7ec7a3f9c15ee36ff8e1b90e99df0b
Visa-Valtteri Pimiä <vp79799@gmail.com>
2013-06-20 21:29:45 +0300

    intro scene pic effect done

--- fsquad.fs
+++ fsquad.fs
@@ -8,6 +8,7 @@ uniform sampler2D texture0;
 uniform float alpha;
 
 uniform float gamma;
+uniform float grid;
 
 void main()
 {
@@ -16,6 +17,36 @@ void main()
     color.a = alpha;
 
     if (gamma > 0.0) color.rgb*=gamma;
+
+    if (grid > 0.0)
+    {
+        vec3 col = vec3(0.0,0.0,0.0);
+
+        float off = -time*2;
+        
+        //really light lines
+        col.g += clamp(ceil(mod(gl_FragCoord.x+off, 5.0)) - 4.0, 4.0, 1.0) * (0.05*grid);
+        col.g += clamp(ceil(mod(gl_FragCoord.y+off, 5.0)) - 4.0, 0.0, 1.0) * (0.05*grid);
+        col.g = clamp(col.g, 0.0, (0.15*grid));
+        
+        //light lines
+        col.g += clamp(ceil(mod(gl_FragCoord.x+off, 15.0)) - 14.0, 0.0, 1.0) * (0.25*grid);
+        col.g += clamp(ceil(mod(gl_FragCoord.y+off, 15.0)) - 14.0, 0.0, 1.0) * (0.25*grid);
+        col.g = clamp(col.g, 0.0, (0.25*grid));
+        
+        //strong lines
+        col.g += clamp(ceil(mod(gl_FragCoord.x+off, 30.0)) - 29.0, 0.0, (1.0*grid));
+        col.g += clamp(ceil(mod(gl_FragCoord.y+off, 30.0)) - 29.0, 0.0, (1.0*grid));
+        col.g = clamp(col.g, 0.0, (1.0*grid));
+
+        if (((color.r + color.b + color.g) / 3.0) > 0.1)
+        {
+            if (color.r / col.g < 0.2) color.rgb = 0.0;
+        }
+        else
+        color.rgb -= col.g*(0.5*cos(time*0.1+(gl_FragCoord.x+gl_FragCoord.y)));
+    }
+
     gl_FragColor = color;
 }
 
--- projector.fs
+++ projector.fs
@@ -7,21 +7,35 @@ uniform float time;
 uniform sampler2D texture0;
 uniform float alpha;
 
+int scroll_trig = 0;
+float trig_time = 0;
 void main()
 {
-	float zoom = time*0.0001;
+	float zoom = atan(time*0.00015);
 
-	if (zoom > 1.0) zoom = 1.0;
+    float scroll = 0.0;
 
-    vec4 color = texture2D(texture0, vec2(gl_FragCoord.x/width*zoom, gl_FragCoord.y/height*zoom));
-    vec4 color3 = texture2D(texture0, vec2(gl_FragCoord.x/width*cos(time+gl_FragCoord.y), gl_FragCoord.y/height*sin(time+gl_FragCoord.x)));
+	if (zoom > 0.95) {
+        zoom = 1.0;
+        scroll_trig = 1;
+    }
+
+    if (scroll_trig == 1)
+    {
+        scroll = -(time-8000)*0.1;
+        if (scroll < -width/4) scroll = -width/4;
+    }
+
+    vec4 color = texture2D(texture0, vec2((scroll+gl_FragCoord.x)/width*zoom, (gl_FragCoord.y)/height*zoom));
+    vec4 color3 = texture2D(texture0, vec2(gl_FragCoord.x/width*cos(time*0.01+gl_FragCoord.y*0.01), gl_FragCoord.y/height*sin(time*0.02+gl_FragCoord.x*0.02)));
+    vec4 color4 = texture2D(texture0, vec2(gl_FragCoord.x/width*tan(time*0.001+gl_FragCoord.x*0.002), gl_FragCoord.y/height*tan(time*0.001+gl_FragCoord.x*0.002)));
 
     color.a = alpha;
     if (color.r > 0.1 && color.g > 0.1 && color.b > 0.1)
     {
 //    	float effu = (0.1*cos(gl_FragCoord.y*0.1+time));
-    	vec4 color2 = vec4(color.r*color3.r, color.g*color3.g, color.b*color3.b, color3.r*color.a*0.1*zoom);
-    	gl_FragColor = color2+(color*zoom*(0.05*cos(time*0.01)));
+    	vec4 color2 = vec4(color.r*color3.r*0.2-color4.r*time*0.0007, color.g*color3.g*0.1-color4.r*time*0.0005, color.b*color3.b*0.2-color4.r*time*0.0004, color3.r*color.a*0.1+abs(zoom));
+    	gl_FragColor = color2+(color*zoom*abs((0.5*sin(time*0.0005))));
     }
 }
 
