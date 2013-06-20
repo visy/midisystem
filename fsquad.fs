0c4600fccd827cd311396c601ebe9487375aa4a0
Visa-Valtteri Pimiä <vp79799@gmail.com>
2013-06-20 13:33:25 +0300

    gamma ramping

--- fsquad.fs
+++ fsquad.fs
@@ -7,12 +7,15 @@ uniform float time;
 uniform sampler2D texture0;
 uniform float alpha;
 
+uniform float gamma;
+
 void main()
 {
     vec4 color = texture2D(texture0, vec2(gl_FragCoord.x/width, gl_FragCoord.y/height));
 
     color.a = alpha;
 
+    if (gamma > 0.0) color.rgb*=gamma;
     gl_FragColor = color;
 }
 
